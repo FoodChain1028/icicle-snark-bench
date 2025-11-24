40ca3c90750a82a33db9b133f2bbdc4749ab8ffd
Jeff Chung <sh1001309@gmail.com>
2025-11-24 15:57:19 +0800

    fix: remove unused script file

--- examples/rust/run_example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run the example with the local backend built by the user
-export ICICLE_BACKEND_INSTALL_DIR=/home/foodchain/tok/icicle-snark/icicle/build/backend
-./target/release/groth16-usage
